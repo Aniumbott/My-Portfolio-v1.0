42a40a4715c72bb2590869cf987e9e739dc01f2b
Aniumbott <aniketnareshkumarrana@gmail.com>
2021-12-05 11:08:09 +0530

    project details added

--- projects.js
+++ projects.js
@@ -9,7 +9,6 @@ const doneProjects = projectsList[1];
 function createProjectCard(card) {
   let projectCard = document.createElement("div");
   projectCard.classList.add("project-card");
-  projectCard.id = card.name;
   let projectCover = document.createElement("div");
   projectCover.classList.add("project-cover");
   let name = document.createElement("h3");
@@ -19,6 +18,9 @@ function createProjectCard(card) {
   image.src = card.screenShot;
   projectCard.appendChild(projectCover);
   projectCard.appendChild(image);
+  projectCard.addEventListener("click", () => {
+    showDetails(card);
+  });
   return projectCard;
 }
 
@@ -53,6 +55,67 @@ function addCards(check) {
   }
 }
 
+function showDetails(card) {
+  // Container
+  let detailsContainer = document.createElement("div");
+  detailsContainer.classList.add("details-popup-container");
+  // main popup
+  let detailsPopUp = document.createElement("div");
+  detailsPopUp.classList.add("details-popup");
+  // close button
+  let closeButton = document.createElement("button");
+  closeButton.innerText = "×";
+  // screenshot
+  let image = document.createElement("img");
+  image.id = "details-screenshot";
+  image.src = card.screenShot;
+  // Details
+  let details = document.createElement("div");
+  details.classList.add("details");
+
+  let name = document.createElement("h1");
+  name.innerText = card.name;
+
+  let description = document.createElement("p");
+  description.innerText = card.dec;
+  // buttons
+  let buttonsContainer = document.createElement("div");
+  buttonsContainer.classList.add("buttons-container");
+
+  let repolink = document.createElement("a");
+  repolink.target = "_blank";
+  repolink.href = card.repolink;
+  let github = document.createElement("img");
+  github.src = "./icons/github.svg";
+
+  let prototype = document.createElement("a");
+  prototype.target = "_blank";
+  prototype.href = card.url;
+  let website = document.createElement("img");
+  website.src = "./icons/prototype.svg";
+
+  // Creating structure
+  repolink.appendChild(github);
+  prototype.appendChild(website);
+  buttonsContainer.appendChild(repolink);
+  if (card.url != "") {
+    buttonsContainer.appendChild(prototype);
+  }
+  details.appendChild(name);
+  details.appendChild(description);
+  details.appendChild(buttonsContainer);
+  detailsPopUp.appendChild(closeButton);
+  detailsPopUp.appendChild(image);
+  detailsPopUp.appendChild(details);
+  detailsContainer.appendChild(detailsPopUp);
+
+  // Close popup
+  closeButton.addEventListener("click", () => {
+    document.querySelector(".details-popup-container").remove();
+  });
+
+  document.querySelector("body").appendChild(detailsContainer);
+}
+
 addCards("ongoing");
 addCards("done");
-// console.log(doneProjects);
